caffe7db8f241d2395e55fa207433ab7b8feb198
Igor Klem <klemigor@gmail.com>
2020-06-18 16:47:55 -0300

    Created model view controller basic code

---
+++ webapp/view/App.view.js
@@ -0,0 +1,28 @@
+sap.ui.jsview("igorklem.github.io.view.Main", {
+
+	/** Specifies the Controller belonging to this View. 
+	 * In the case that it is not implemented, or that "null" is returned, this View does not have a Controller.
+	 * @memberOf controller.Main
+	 */
+	getControllerName: function() {
+		return "igorklem.github.io.controller.Main";
+	},
+
+	/** Is initially called once after the Controller has been instantiated. It is the place where the UI is constructed. 
+	 * Since the Controller is given to this method, its event handlers can be attached right away.
+	 * @memberOf controller.Main
+	 */
+	createContent: function(oController) {
+		var oPage = new sap.m.Page({
+			title: "{i18n>title}",
+			content: []
+		});
+
+		var app = new sap.m.App("myApp", {
+			initialPage: "oPage"
+		});
+		app.addPage(oPage);
+		return app;
+	}
+
+});
